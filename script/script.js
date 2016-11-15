ee07e03a7e64f31023c6d7ba5166b87c76c99eee
mitinsharma <er.mitinsharma@gmail.com>
2016-11-15 10:14:05 -0500

    Filters, table sorting

--- script/script.js
+++ script/script.js
@@ -73,5 +73,45 @@ var myApp = angular
                 technology.dislikes++;
             }
 
+
+            var filter_emps = [
+                {fname:'mitin',lname:'sharma',gender:'male',dob:new Date("January 12, 1993"),salary:15152.325},
+                {fname:'nitin',lname:'bhardwaj',gender:'female',dob:new Date("January 12, 1994"),salary:35452.425},
+                {fname:'rahul',lname:'verma',gender:'female',dob:new Date("January 12, 1993"),salary:17452.265},
+                {fname:'akash',lname:'patel',gender:'male',dob:new Date("January 12, 1993"),salary:15152.225},
+                {fname:'saajan',lname:'kapoor',gender:'female',dob:new Date("January 12, 1993"),salary:7552.125},
+                {fname:'saajan',lname:'kapoor',gender:'female',dob:new Date("January 12, 1993"),salary:1452.125}
+
+            ];
+
+            $scope.filter_emps = filter_emps;
+
+            $scope.rowLimit = 3;
+            $scope.sortColumn = "fname";
+            $scope.decOrder = false;
+            $scope.fname_arrowDir = 'down';
+            $scope.dob_arrowDir = 'down';
+            $scope.salary_arrowDir = 'down';
+
+            $scope.changeOrder = function($col){
+                /*
+                if($col==$scope.sortColumn)
+                {
+                    if($scope.decOrder==false)$scope.decOrder = true;
+                    else $scope.decOrder=false;
+                }
+                else
+                {
+                    $scope.sortColumn=$col;
+                    $scope.decOrder=false;
+                }
+                */
+                /** Same written in smart code**/
+                $scope.decOrder = ($col==$scope.sortColumn)? !$scope.decOrder : false;
+                $scope.fname_arrowDir = ($scope.decOrder && $col == 'fname') ? 'up' : 'down';
+                $scope.dob_arrowDir = ($scope.decOrder && $col == 'dob') ? 'up' : 'down';
+                $scope.salary_arrowDir = ($scope.decOrder && $col == 'salary') ? 'up' : 'down';
+                $scope.sortColumn=$col;
+            }
         });
 
